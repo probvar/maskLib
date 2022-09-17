81823b1f861a30f609657d792eee330a4cab819d
grangerave <grangerave@yahoo.com>
2022-09-17 12:19:12 -0500

    Fixed RBar Definitions

--- dcLib.py
+++ dcLib.py
@@ -10,8 +10,8 @@ Library for drawing standard components for DC measurements (Four probe resistan
 import maskLib.MaskLib as m
 from dxfwrite import DXFEngine as dxf
 from dxfwrite import const
-from microwaveLib import Strip_straight, Strip_stub_open
-from microwaveLib import CPW_stub_open,CPW_stub_short,CPW_straight
+from maskLib.microwaveLib import Strip_straight, Strip_stub_open
+from maskLib.microwaveLib import CPW_stub_open,CPW_stub_short,CPW_straight
 
 
 # ===============================================================================
@@ -41,7 +41,7 @@ class Rbar(m.Chip):
         self.add(dxf.rectangle(self.centered((bar_offs + pad_sep/2 +bar_width/2,0)),self.width/2 - bar_offs - pad_x - pad_sep/2 - bar_width/2,bar_length,valign=const.MIDDLE,bgcolor=wafer.bg(),layer=wafer.defaultLayer))
         self.add(dxf.rectangle(self.centered((-bar_offs - pad_sep/2 -bar_width/2,0)),self.width/2 - bar_offs - pad_x - pad_sep/2 - bar_width/2,bar_length,halign=const.RIGHT,valign=const.MIDDLE,bgcolor=wafer.bg(),layer=wafer.defaultLayer))
         
-def ResistanceBarBilayer(self,chip,structure,length=1500,width=40,pad=600,gap=50,r_out=None,secondlayer='SECONDLAYER',bgcolor=None):
+def ResistanceBarBilayer(chip,structure,length=1500,width=40,pad=600,gap=50,r_out=None,secondlayer='SECONDLAYER',bgcolor=None):
     #write a resistance bar centered on the structure or position specified. Defaults to pointing in direction of current structure
     def struct():
         if isinstance(structure,m.Structure):
@@ -57,20 +57,20 @@ def ResistanceBarBilayer(self,chip,structure,length=1500,width=40,pad=600,gap=50
     
     struct().shiftPos(-length/2-pad-gap)
     srBar=struct().clone(defaults={'w':pad,'s':gap,'r_out':gap})
-    Strip_stub_open(self,srBar,flipped=True,w=pad+2*gap)
+    Strip_stub_open(chip,srBar,flipped=True,w=pad+2*gap)
     srBar2 = srBar.cloneAlong()
-    Strip_straight(self,srBar,pad,w=pad+2*gap)
-    Strip_stub_open(self,srBar,flipped=False,w=pad+2*gap)
-    Strip_straight(self, srBar, length-2*gap, w=width+2*gap)
-    Strip_stub_open(self,srBar,flipped=True,w=pad+2*gap)
-    Strip_straight(self,srBar,pad,w=pad+2*gap)
-    Strip_stub_open(self,srBar,w=pad+2*gap)
+    Strip_straight(chip,srBar,pad,w=pad+2*gap)
+    Strip_stub_open(chip,srBar,flipped=False,w=pad+2*gap)
+    Strip_straight(chip, srBar, length-2*gap, w=width+2*gap)
+    Strip_stub_open(chip,srBar,flipped=True,w=pad+2*gap)
+    Strip_straight(chip,srBar,pad,w=pad+2*gap)
+    Strip_stub_open(chip,srBar,w=pad+2*gap)
     
-    Strip_straight(self,srBar2,pad,w=pad,layer=secondlayer)
-    Strip_straight(self, srBar2, length, w=width,layer=secondlayer)
-    Strip_straight(self,srBar2,pad,w=pad,layer=secondlayer)
+    Strip_straight(chip,srBar2,pad,w=pad,layer=secondlayer)
+    Strip_straight(chip, srBar2, length, w=width,layer=secondlayer)
+    Strip_straight(chip,srBar2,pad,w=pad,layer=secondlayer)
     
-def ResistanceBarNegative(self,chip,structure,length=1500,width=40,pad=600,gap=50,r_out=None,secondlayer='SECONDLAYER',bgcolor=None):
+def ResistanceBarNegative(chip,structure,length=1500,width=40,pad=600,gap=50,r_out=None,bgcolor=None):
     #write a resistance bar centered on the structure or position specified. Defaults to pointing in direction of current structure
     def struct():
         if isinstance(structure,m.Structure):
@@ -86,15 +86,15 @@ def ResistanceBarNegative(self,chip,structure,length=1500,width=40,pad=600,gap=5
     
     struct().shiftPos(-length/2-pad-gap)
     srBar=struct().clone(defaults={'w':pad,'s':gap,'r_out':r_out})
-    CPW_stub_open(self,srBar,flipped=True)
-    CPW_straight(self,srBar,pad)
-    CPW_stub_short(self,srBar,flipped=False,w=width,s=(pad+2*gap-width)/2,curve_ins=False)
-    CPW_straight(self, srBar, length-2*gap, w=width)
-    CPW_stub_short(self,srBar,flipped=True,w=width,s=(pad+2*gap-width)/2,curve_ins=False)
-    CPW_straight(self,srBar,pad)
-    CPW_stub_open(self,srBar)
+    CPW_stub_open(chip,srBar,flipped=True)
+    CPW_straight(chip,srBar,pad)
+    CPW_stub_short(chip,srBar,flipped=False,w=width,s=(pad+2*gap-width)/2,curve_ins=False)
+    CPW_straight(chip, srBar, length-2*gap, w=width)
+    CPW_stub_short(chip,srBar,flipped=True,w=width,s=(pad+2*gap-width)/2,curve_ins=False)
+    CPW_straight(chip,srBar,pad)
+    CPW_stub_open(chip,srBar)
     
-def ResistanceBar(self,chip,structure,length=1500,width=40,pad=600,r_out=50,secondlayer='SECONDLAYER',bgcolor=None):
+def ResistanceBar(chip,structure,length=1500,width=40,pad=600,r_out=50,secondlayer='SECONDLAYER',bgcolor=None):
     #write a resistance bar centered on the structure or position specified. Defaults to pointing in direction of current structure
     def struct():
         if isinstance(structure,m.Structure):
@@ -109,6 +109,6 @@ def ResistanceBar(self,chip,structure,length=1500,width=40,pad=600,r_out=50,seco
     struct().shiftPos(-length/2-pad)
     srBar=struct().clone(defaults={'w':pad,'r_out':r_out})
     
-    Strip_straight(self,srBar,pad,w=pad,layer=secondlayer)
-    Strip_straight(self, srBar, length, w=width,layer=secondlayer)
-    Strip_straight(self,srBar,pad,w=pad,layer=secondlayer)
+    Strip_straight(chip,srBar,pad,w=pad,layer=secondlayer)
+    Strip_straight(chip, srBar, length, w=width,layer=secondlayer)
+    Strip_straight(chip,srBar,pad,w=pad,layer=secondlayer)
